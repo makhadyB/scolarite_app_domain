7f10c859a3fe6e6de2227f0c550948117c26c35f
makhadyB <makhadybocoum06@gmail.com>
2026-02-10 12:20:42 +0000

    feat:ajout fichier docker

--- src/main/java/com/uahb/scolarite/app/domain/entity/annee/Statut.java
+++ src/main/java/com/uahb/scolarite/app/domain/entity/annee/Statut.java
@@ -1,8 +1,6 @@
 package com.uahb.scolarite.app.domain.entity.annee;
 
-import java.util.List;
-
-public enum Statut implements List<Statut> {
+public enum Statut {
     BROUILLON, PUBLIEE, INSCRIPTIONS_OUVERTES, INSCRIPTION_SUSPENDUES, INSCRIPTIONS_FERMEES, CLOTUREE;
 
 
