bb48d903cbd671656e64cf447fed2790437328fa
makhadyB <makhadybocoum06@gmail.com>
2026-02-13 18:14:07 +0000

    docker file

--- src/main/java/com/uahb/scolarite/app/ScolariteAppApplication.java
+++ src/main/java/com/uahb/scolarite/app/ScolariteAppApplication.java
@@ -2,12 +2,25 @@ package com.uahb.scolarite.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class ScolariteAppApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ScolariteAppApplication.class, args);
 	}
 
+}
+@RestController
+class HelloWordController
+{
+	@GetMapping("/")
+	public  String sayHello()
+	{
+		return "hello word";
+	}
 }
